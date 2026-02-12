7760a9c40c398eecf99e8c46f34ca7bd6f1332f3
skooter500 <skooter500@gmail.com>
2026-02-12 15:23:13 +0000

    explosion color

--- 3dgame/gun.gd
+++ 3dgame/gun.gd
@@ -6,6 +6,8 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("fire"):
 		var p = projectile_scene.instantiate()
 		get_tree().root.add_child(p)
+		$AudioStreamPlayer3D.pitch_scale = randf_range(0.8, 1.2)
 		$AudioStreamPlayer3D.play()
+		
 		p.global_position = $bullet_spawn.global_position
 		p.global_rotation = $bullet_spawn.global_rotation
--- 3dgame/projectile.gd
+++ 3dgame/projectile.gd
@@ -1,11 +1,20 @@
 extends Area3D
 
 @export var speed:float = 20
+@export var explosion_scene:PackedScene
 
 func _physics_process(delta: float) -> void:
 	translate(Vector3.FORWARD * speed * delta)
 
 
 func _on_body_entered(body: Node3D) -> void:
+	body.queue_free()
+	self.queue_free()
+	var explosion = explosion_scene.instantiate()
+	get_tree().root.add_child(explosion)
+	var col = body.get_node("MeshInstance3D").get_surface_override_material(0).albedo_color	
+	explosion.emitting = true
+	explosion.get_material_override().albedo_color = col
+	explosion.global_position = self.global_position
 	print("I hit something!!")
 	pass # Replace with function body.
